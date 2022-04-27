8682c3df9a4ce125462a403077fb3ff10582024e
checho <sergioangel030407@gmail.com>
2022-04-27 13:17:44 -0500

    Grafica solido revolucion en eje y, falta que muestre volumen

--- integrales/solidoderevolucionif.py
+++ integrales/solidoderevolucionif.py
@@ -1,5 +1,7 @@
 import streamlit as st
 from integrales.solidoderevolucion import comenzarX
+import numpy as np
+import matplotlib.pyplot as plt
 
 def solidoderevolucion_if():
     confirmar = True
@@ -16,4 +18,31 @@ def solidoderevolucion_if():
             st.success(f'Volumen: {resultado}') 
 
         elif eje == 'y':
-            pass
+
+            np.seterr(divide='ignore', invalid='ignore')
+
+            fig = plt.figure()
+            ax = fig.add_subplot(111, projection='3d')
+
+
+            ll, ul = 0, 1 
+            u = np.linspace(ll, ul, 60)
+            v = np.linspace(0, 2*np.pi, 60)
+            U, V = np.meshgrid(u, v)
+
+            Z = U
+
+            X = np.sqrt(Z)*np.cos(V)
+            Y = np.sqrt(Z)*np.sin(V)
+
+            ax.set_xlabel('Y axis')
+            ax.set_ylabel('X axis')
+            ax.set_zlabel('Z axis')
+
+            ax.plot_surface(X, Y, Z, cmap=plt.cm.YlGnBu_r)
+
+            #plt.show()
+            st.pyplot(plt,figsize=(2, 2))
+
+            y = function
+            
